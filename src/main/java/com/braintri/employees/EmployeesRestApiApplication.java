70c483577fe6d4e952f7635002134c7a462cfc8a
sochinho <sochinho@hotmail.com>
2018-03-26 22:06:49 +0200

    master commit

---
+++ src/main/java/com/braintri/employees/EmployeesRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.braintri.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesRestApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/braintri/employees/EmployeesRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.braintri.employees;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeesRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
